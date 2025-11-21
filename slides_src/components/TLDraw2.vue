ebeaee698e7df4418cc20b3317912d1fb9a82b67
Krit Chomaitong <luckkrit@hotmail.com>
2025-11-21 15:16:19 +0700

    add ch3

---
+++ slides_src/components/TLDraw2.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, toRaw } from 'vue'
+import { lockShortcuts } from '@slidev/client/state/storage.ts'
+import { onMounted, onUnmounted } from 'vue'
+import { useMagicKeys, whenever } from '@vueuse/core'
+import { onSlideEnter } from '@slidev/client'
+const { escape } = useMagicKeys()
+let release = ref(null);
+const props = defineProps(['doc', 'class'])
+onUnmounted(() => {
+    handleRelaseLock()
+})
+
+onSlideEnter(() => {
+    console.log("Slide Enter")
+    handleLock()
+})
+
+whenever(escape, () => {
+    console.log("Escape key")
+    if (release.value !== null) {
+        handleRelaseLock()
+    } else if (!release.value) {
+        handleLock()
+    }
+})
+
+
+function handleLock() {
+    release.value = lockShortcuts()
+    console.log("lock")
+}
+function handleRelaseLock() {
+    release?.value?.()
+    release.value = null
+    console.log("unlock")
+}
+
+
+</script>
+
+<template>
+    <div>
+        <p class="text-sm opacity-50">💡 Press ESC to <span class="text-red-500" v-if="release">Unlock
+                🔑</span><span class="text-green-500" v-else>Lock 🔒</span> slide navigation</p>
+
+        <tldraw :class="`${props.class}`" :doc="`${props.doc}`"></tldraw>
+    </div>
+</template>
---
+++ slides_src/pages/ch3.md
@@ -0,0 +1,34 @@
+---
+layout: section
+---
+
+# Names, Bindings, and Scopes
+
+---
+
+# Contents
+
+<Toc minDepth="2"/>
+
+---
+
+## Introduction
+
+- Programming Languages
+
+	- Imperative programming languages
+		- C/C++, Java, C#, etc.
+
+	- Imperative/multi-paradigm programming languages
+		- C/C++, PHP, C#, Java, Kotlin, Python
+
+	- Pure functional programming languages
+		- Haskell, Lisp, Scheme, Clojure, Erlang, F#, OCaml, Scala
+
+Reference: [https://en.wikipedia.org/wiki/List_of_programming_languages_by_type#Imperative_languages](https://en.wikipedia.org/wiki/List_of_programming_languages_by_type)
+
+---
+
+<TLDraw2 :class="'inset-0 w-full h-full'" :doc="'tldraw/memory_cell.tldr'" ></TLDraw2>
+
+
