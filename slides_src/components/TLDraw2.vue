96b31ff99aaa1142371f38395df47202f03385dc
Krit Chomaitong <luckkrit@hotmail.com>
2025-11-23 11:39:03 +0700

    update ch3

--- slides_src/components/TLDraw2.vue
+++ slides_src/components/TLDraw2.vue
@@ -7,6 +7,14 @@ import { onSlideEnter } from '@slidev/client'
 const { escape } = useMagicKeys()
 let release = ref(null);
 const props = defineProps(['doc', 'class'])
+const snapshot = ref(null)
+const base = import.meta.env.BASE_URL
+const isDev = import.meta.env.DEV
+console.log(base)
+onMounted(async () => {
+    const response = await fetch(`${base}${props.doc}`)
+    snapshot.value = await response.json()
+})
 onUnmounted(() => {
     handleRelaseLock()
 })
@@ -44,6 +52,7 @@ function handleRelaseLock() {
         <p class="text-sm opacity-50">💡 Press ESC to <span class="text-red-500" v-if="release">Unlock
                 🔑</span><span class="text-green-500" v-else>Lock 🔒</span> slide navigation</p>
 
-        <tldraw :class="`${props.class}`" :doc="`${props.doc}`"></tldraw>
+        <tldraw v-if="isDev" :class="`${props.class}`" :doc="`${props.doc}`"></tldraw>
+        <tldraw v-if="!isDev" :class="`${props.class}`" :snapshot="`${snapshot}`"></tldraw>
     </div>
 </template>
